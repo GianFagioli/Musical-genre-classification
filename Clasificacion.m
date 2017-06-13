28c21166cf7fe9fec6453f69f8142a56a9aa1187
Gian <gian_fagioli@hotmail.com>
2017-06-13 16:13:12 -0300

    cambios

--- Clasificacion.m
+++ Clasificacion.m
@@ -3,7 +3,7 @@ function [] = Clasificacion(red)
 tamVent = 1024; %Tamaño de la ventana
 paso = tamVent; %Overlap de ventanas: Si es igual al tamaño de la ventana no hay overlap
 
-[s_cumbia, fm_c] = audioread(strcat('Canciones/rap9_modificado.wav'));
+[s_cumbia, fm_c] = audioread(strcat('Canciones/reggae3_modificado.wav'));
 desconocido(1,:) = extraerCaract(s_cumbia,tamVent,paso,fm_c);
 
 resultado = predict (red, desconocido);
--- entrenamiento.m
+++ entrenamiento.m
@@ -1,26 +1,34 @@
-function [red] = entrenamiento()
+function [red,carac] = entrenamiento()
 
 tamVent = 1024; %Tamaño de la ventana
 paso = tamVent; %Overlap de ventanas: Si es igual al tamaño de la ventana no hay overlap
 fm_clasico=zeros(8,1); %vector columna con las frecuencias de muestreo
-carac=zeros(24,24);
+carac=zeros(32,24);
+
 for i=1:8
     [s_clasico, fm_clasico(i,1)] = audioread(strcat('Canciones/clasico' , int2str(i),'_modificado.wav'));
     carac(i,:) = extraerCaract(s_clasico,tamVent,paso,fm_clasico(i,1));
 end
 
 fm_rap=zeros(8,1);
-for j=1:8
+for j=1:6
    [s_rap, fm_rap(j,1)] = audioread(strcat('Canciones/rap' , int2str(j),'_modificado.wav'));
    carac(i+j,:) = extraerCaract(s_rap,tamVent,paso,fm_rap(j,1));
 end
 
-fm_cumbia=zeros(8,1);
+fm_folklore=zeros(8,1);
 for k=1:8
-    [s_cumbia, fm_cumbia(k,1)] = audioread(strcat('Canciones/cumbia' , int2str(k),'_modificado.wav'));
-    carac(i,:) = extraerCaract(s_cumbia,tamVent,paso,fm_cumbia(k,1));
+    [s_folbia, fm_folklore(k,1)] = audioread(strcat('Canciones/folklore' , int2str(k),'_modificado.wav'));
+    carac(i+j+k,:) = extraerCaract(s_folbia,tamVent,paso,fm_folklore(k,1));
 end
 
-etiqueta= [ 'cla'; 'cla'; 'cla';'cla'; 'cla'; 'cla';'cla'; 'cla'; 'rap'; 'rap'; 'rap';'rap'; 'rap'; 'rap';'rap'; 'rap'; 'cum'; 'cum'; 'cum';'cum'; 'cum'; 'cum';'cum'; 'cum';];
+fm_reggae=zeros(8,1);
+for l=1:8
+    [s_folbia, fm_reggae(l,1)] = audioread(strcat('Canciones/reggae' , int2str(l),'_modificado.wav'));
+    carac(i+j+k+l,:) = extraerCaract(s_folbia,tamVent,paso,fm_reggae(l,1));
+end
+
+etiqueta= [ 'cla'; 'cla'; 'cla';'cla'; 'cla'; 'cla';'cla'; 'cla'; 'rap'; 'rap'; 'rap';'rap'; 'rap'; 'rap';'rap'; 'rap'; 'fol'; 'fol'; 'fol';'fol'; 'fol'; 'fol';'fol'; 'fol'; 'reg'; 'reg'; 'reg'; 'reg'; 'reg'; 'reg'; 'reg'; 'reg';];
+
 
 red = fitcdiscr(carac,etiqueta);
